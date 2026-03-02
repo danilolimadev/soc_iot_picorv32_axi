d4a55f6d2d79045b92b7aa968e8ccb6a11bb314a
emenezes <menezes.ime@gmail.com>
2026-03-01 21:37:46 -0300

    Software para leitura da uart e escrita em outros periféricos

--- test/tb_soc_top.v
+++ test/tb_soc_top.v
@@ -117,11 +117,11 @@ module soc_tb;
 
     $display("[TB] Enviando byte 'B'");
     uart_send_byte(8'h42);
-    #100000;
+    #1300000;
 
     $display("[TB] Enviando byte 'C'");
     uart_send_byte(8'h43);
-    #100000;
+    #200000;
 
     $display("[TB] Enviando byte 'D'");
     uart_send_byte(8'h44);
